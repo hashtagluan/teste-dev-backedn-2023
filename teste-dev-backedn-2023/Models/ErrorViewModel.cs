3795596605c3e1b3d9ac021d17ba4516f7ec53f7
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2023-12-04 22:39:14 -0300

    Adicionar arquivos de projeto.

---
+++ teste-dev-backedn-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teste_dev_backedn_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
